93dd13f08ef2e301c214e3eb529dc7f9a5ecc156
Soorria Saruva <soorria.ss@gmail.com>
2022-12-05 21:43:49 +1100

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>MDX City</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        MDX City
+      </main>
+    </>
+  );
+};
+
+export default Home;
